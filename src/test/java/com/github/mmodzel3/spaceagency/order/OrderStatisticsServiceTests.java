b64b5731481cc82af41cc5bd114c4aa61c5055b5
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2021-08-14 08:35:24 +0200

    Added test to OrderStatisticsService.

--- src/test/java/com/github/mmodzel3/spaceagency/order/OrderStatisticsServiceTests.java
+++ src/test/java/com/github/mmodzel3/spaceagency/order/OrderStatisticsServiceTests.java
@@ -1,13 +1,11 @@
 package com.github.mmodzel3.spaceagency.order;
 
 import com.github.mmodzel3.spaceagency.product.Product;
-import com.github.mmodzel3.spaceagency.product.ProductNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,6 +15,13 @@ class OrderStatisticsServiceTests extends OrderTestsAbstract {
 
     @Autowired
     OrderStatisticsService orderStatisticsService;
+
+    @Test
+    void whenGetMostOrderedProductAndNoProductsThenGotNothing() {
+        Optional<Product> possibleMostOrderedProduct = orderStatisticsService.getMostOrderedProduct();
+
+        assertFalse(possibleMostOrderedProduct.isPresent());
+    }
 
     @Test
     void whenGetMostOrderedProductThenGotIt() {
